04cb6eab5607992e9048a46de2ce715c4eae09d7
Ne-protiv <andreibelocopytov20134@gmail.com>
2024-04-11 02:27:01 +0300

    result

---
+++ src/Task.java
@@ -0,0 +1,15 @@
+public class Task {
+    public static void main(String[] args) {
+        String A = "a";
+        char a = A.charAt(0);
+        System.out.println(a);
+        char charExpression = charExpression(a);
+        System.out.println(charExpression);
+    }
+    public static char charExpression(int a) {
+        String slash = "/";
+        char Slash = slash.charAt(0);
+        System.out.println(Slash);
+        return (char) (a + Slash);
+    }
+}
